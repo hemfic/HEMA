34e4b9a1d8ad031a22e6623dcf12c40d950f4438
Eric <ericreesor@outlook.com>
2017-09-14 15:50:54 -0400

    update/experimentation
    experimentation with movement of objects. removal of feder model pending updated model exportation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public float torque;
 
 	private Rigidbody rb;
 
@@ -15,16 +16,27 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
+		float reset = Input.GetAxis ("Cancel");
 		float moveX = Input.GetAxis ("Horizontal");
 		float moveY = Input.GetAxis ("Vertical");
 		float moveUp = Input.GetAxis ("Fire1");
 		float moveDown = Input.GetAxis ("Fire2");
+		float spinX = Input.GetAxis ("Jump");
+		float spinY = Input.GetAxis ("Mouse X");
+		float spinZ = Input.GetAxis ("Mouse Y");
 		Vector3 movement = new Vector3 (moveX, 0.0f, moveY);
+		Vector3 origin = new Vector3 (0.0f,0.0f,0.0f);
 		if (moveUp!=0.0f)
 			movement=new Vector3 (moveX, moveUp, moveY);
 		else if (moveDown!=0.0f)
 			movement=new Vector3 (moveX, -1.0f*moveDown, moveY);
+		print (reset);
+		if (reset != 0)
+			rb.position = origin;
 
 		rb.AddForce (movement * speed);
+		rb.AddTorque (transform.up * torque * spinX);
+		rb.AddTorque (transform.right * torque * spinY);
+		rb.AddTorque (transform.forward * torque * spinZ);
 	}
 }
